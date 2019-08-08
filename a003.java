3a5480a8e5a3850cf6f95227f612a1c619caff3b
ChamaTW <39480001+ChamaTW@users.noreply.github.com>
2019-08-08 17:28:16 +0800

    Update a003.java

--- a003.java
+++ a003.java
@@ -5,23 +5,23 @@ public class A003 {
   int M=0,D=0,S=0;
   @SuppressWarnings("resource")
   Scanner cin= new Scanner(System.in);
-  while(cin.hasNext()){
+       while(cin.hasNext()){
    M =cin.nextInt();
    D =cin.nextInt();
    S=(M*2+D)%3;
-   switch (S){
-   case 0:
+        switch (S){
+     case 0:
     System.out.println("普通");
-    break;
-   case 1:
+           break;
+     case 1:
     System.out.println("吉");
-    break;
+         break;
    case 2:
     System.out.println("大吉");
-    break;
+         break;
    default: 
     break;
-   }
-  }
- }
+              }
+        }
+    }
 }
